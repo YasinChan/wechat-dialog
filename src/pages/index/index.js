30b96e4eaabb46c99f40df4c15244bcc83332587
yasinchan <yasinchan2016@gmail.com>
2018-08-19 22:59:04 +0800

    update

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -8,35 +8,37 @@ import {pagify, MyPage} from 'base/'
 // 把这个 class 转化成 微信的 Page 参数，并且注入全局 store
 @pagify()
 export default class extends MyPage {
-  data = {
-    count: 10,
-    npmToast: false,
-    demoToast: false,
-    motto: ''
+  data = {  // 颠倒一下 selectData2 和 selectData1 查看效果
+    selectData2: {
+      type: '单选',
+      item: ['选项一', '选项二', '选项三', '选项四', '选项五', '选项六']
+    },
+    selectData1: {
+      type: '多选',
+      item: ['选项一', '选项二', '选项三', '选项四', '选项五', '选项六']
+    },
+    multSelected: []  // 用来记录多选里面的已选择的选项
   }
 
-  onShow() {
-    /**
-     * setDataSmart 和 setData 基本一样，但 setDataSmart 内部做过优化：
-     *
-     * 1. 支持双向绑定
-     * 2. 页面隐藏是会缓存 data，等到页面显示的时候再 set
-     */
-    this.setDataSmart({motto: 'See you again, Try to click me!'})
+  clickSelect(e) {  // 单选选择时候出发的事件（也就是会判断当前选项从而跳转到相应的下一个选项里面）
+    let currentItem = e.currentTarget.dataset.item;
+    let currentIndex = e.currentTarget.dataset.index;
+    console.log(`当前点击的item是：${currentItem}`);
+    console.log(`当前点击的index是：${currentIndex}`);
+    // TODO 跳转判断逻辑
   }
 
-  gotoLogsPage() {
-    // 跳转到 logs 页面
-    this.app.$url.logs.go({id: 1})
-    this.setDataSmart({motto: '开始跳转到 logs 页面'})
+  clickMultSelect(e) {  // 多选选择适合触发的事件（给 multselected 赋值）
+    let currentItem = e.currentTarget.dataset.item;
+    let multSelected = this.data.multSelected || []
+    multSelected.push(currentItem)
+    this.setDataSmart({
+      multSelected: multSelected
+    })
   }
 
-  toggleDemoToast() {
-    this.setDataSmart({demoToast: !this.data.demoToast})
-  }
-
-  increase() {
-    this.setDataSmart({count: this.data.count + 1})
+  clickConfirm() {  // 多选后点击确认后跳转到相应的页面
+    // TODO 跳转判断逻辑
   }
 
   restart() {
